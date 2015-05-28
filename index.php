ea27cfebd068c0edf6a3183ab1ca2d4ec643d466
ricardo acevedo <ricardojracevedo@yahoo.com>
2015-05-28 15:01:14 -0700

    final project finished

--- index.php
+++ index.php
@@ -4,12 +4,23 @@
 </head>
 <body>
 <?php
+
+
+ 
+
 include 'calendar.php';
  
 $calendar = new Calendar();
  
 echo $calendar->show();
 ?>
+    
+     <div class="col-xs-2">
+                <nav class="list-group">
+                    <!--these are for the links -->
+                    <a class="list-group-item" href="calendar.html"> sign up</a>
+                </nav>                   
+            </div>
     
 </body>
 </html> 
